2ef8d807f8616b9fa5c788a8ccbbd332dca3b247
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2024-09-11 14:11:45 +0630

    post request

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://kaungmyatthway1997:pTDyRn7rTyh8i9mv@cluster0.ya3n0.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0"
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,53 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/',(request,response) => {
-    return response.status(234).send('Welcome To MERN Stack Tutorial');
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-}) 
+app.post("/books", async(request, response) => {
+    console.log(request.body);
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required field",
+      });
+    }
+
+    const newBook = {
+        title : "request.body.title",
+        author : "request.body.author",
+        publishYear : 123
+    }
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
