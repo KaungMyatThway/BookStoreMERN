bd7379b395750bb5830967ffaf1de64c2ad20440
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2024-09-11 11:35:58 +0630

    add route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/',(request,response) => {
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
-})
+}) 
