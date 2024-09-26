d802f8f467f7df25da83241135693857c6040696
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2024-09-26 13:27:49 +0630

    CRUD process add

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,26 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import { Book } from "./models/bookModel.js"; 
+import booksRoute from "./routes/bookRoute.js";
 
 const app = express();
 
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin:'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders : ['Content-Type'],
+//   })
+// )
+
 app.get("/", (request, response) => {
   return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
-app.post("/books", async(request, response) => {
-    console.log(request.body);
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: "Send all required field",
-      });
-    }
-
-    const newBook = {
-        title : "request.body.title",
-        author : "request.body.author",
-        publishYear : 123
-    }
-
-    const book = await Book.create(newBook);
-
-    return response.status(201).send(book);
-
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
+app.use('/books',booksRoute);
 
 mongoose
   .connect(mongoDBURL)
---
+++ backend/routes/bookRoute.js
@@ -0,0 +1,109 @@
+import express from "express";
+import { Book } from "../models/bookModel.js";
+
+
+const router = express.Router();
+
+//books create
+router.post("/", async(request, response) => {
+    try {
+      if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+      ) {
+        return response.status(400).send({
+          message: "Send all required field",
+        });
+      }
+  
+      const newBook = {
+          title : request.body.title,
+          author : request.body.author,
+          publishYear : request.body.publishYear
+      }
+  
+      const book = await Book.create(newBook);
+  
+      return response.status(201).send(book);
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  //get all books 
+  router.get("/", async(request, response) => {
+    try {
+      const books = await Book.find({});
+  
+      return response.status(200).json({
+        count : books.length,
+        data : books
+      });
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  //get book by list 
+  router.get("//:id", async(request, response) => {
+    try {
+      const { id } = request.params;
+      const book = await Book.findById(id);
+  
+      return response.status(200).json(book);
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  // update book data
+  router.put("//:id", async(request, response) => {
+    try {
+      if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+      ) {
+        return response.status(400).send({
+          message: "Send all required field",
+        });
+      }
+  
+      const {id} = request.params;
+  
+      const result = await Book.findByIdAndUpdate(id,request.body);
+  
+      if(!result) {
+        return response.status(404).json({message:'Book not found'});
+      }
+  
+      return response.status(200).send({message: "Book Updated Successfully"});
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  //delete books 
+  router.delete("/:id", async(request, response) => {
+    try {
+      const { id } = request.params;
+      const book = await Book.findByIdAndDelete(id);
+  
+      return response.status(200).json({message: "Book delete successfully"});
+  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+
+  export default router;
